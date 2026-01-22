f92930fabddc2d067256328f8d4e91cb3c6cf7b7
Bar <barrcohen900@gmail.com>
2026-01-22 22:49:12 +0000

    23/01/2026 00:49

--- client.cpp
+++ client.cpp
@@ -13,22 +13,53 @@ bool recv_line(int fd, std::string& out) {
     char c;
     while (true) {
         ssize_t n = recv(fd, &c, 1, 0);
-        if (n <= 0) return false;
-        if (c == '\n') break;
+        if (n <= 0) return false; // if the connection is closed
+        if (c == '\n') break; // if we reach end of line
         out.push_back(c);
     }
     return true;
 }
 
-// Send the entire message
+// Send the entire message makes sure all data is sent in the tcp protocol
 void send_all(int fd, const std::string& msg) {
     size_t sent = 0;
     while (sent < msg.size()) {
         ssize_t n = send(fd, msg.data() + sent, msg.size() - sent, 0);
-        if (n <= 0) return;
+        if (n <= 0) return; // problem with sending
         sent += n;
     }
 }
-int main(){
+void client_thread(){
+    // Set up client socket
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
 
+    if (fd <0) return; // if there is an error creating the socket
+
+    sockaddr_in srv{};
+    srv.sin_family = AF_INET;
+    // the port and address should match the server settings
+    srv.sin_port = htons(8080);
+    // localhost address
+    inet_pton(AF_INET, "127.0.0.1", &srv.sin_addr);
+    // Handshake with the server
+    if (connect(fd, (sockaddr*)&srv, sizeof(srv))<0) {
+        std::cout << "connect error" << std::endl;
+        close(fd); // close the socket if there is an error
+        return;
+    }
+    std::string st;
+
+    while(true){
+        // Get user input(command)
+        std::getline(std::cin,st);
+        st+='\n'; // the rules of the project is to end each command with \n
+        // Send command to server
+        send_all(fd,st);
+        
+
+    }
+}
+int main(){
+    client_thread();
+    return 0;
 }
--- server.cpp
+++ server.cpp
@@ -16,14 +16,18 @@ bool recv_line(int fd, std::string& out) {
     char c;
     while (true) {
         ssize_t n = recv(fd, &c, 1, 0);
+        // if client disconnects
         if (n <= 0) return false;
+        // if we reach end of line
         if (c == '\n') break;
+        // append character to output
         out.push_back(c);
     }
     return true;
 }
 
 // Send the entire message
+// the loop make sure tat all data is sent in the tcp protocol
 void send_all(int fd, const std::string& msg) {
     size_t sent = 0;
     while (sent < msg.size()) {
@@ -31,43 +35,47 @@ void send_all(int fd, const std::string& msg) {
         if (n <= 0) return;
         sent += n;
     }
+
 }
+// Check if a string is a number
 bool is_number(const std::string& s) {
     for (char c : s) {
         if (!isdigit(c)) return false;
     }
     return true;
 }
-
+// Handle client connection
 void handle_client(int client_fd, InventoryManager& inventory) {
-    bool is_authenticated=false; 
-    std::string username;   
-    
+    bool is_authenticated=false; //check if the user did Hello 
+    std::string username;   //UserName Name
+    // Process client commands
     while(true){
-        bool check_username=true;
-        std::string line;
-        if(!recv_line(client_fd, line)) break;
+        bool check_username=true; //to valid the username (while we run we can see if we realy need another check if the user is authenticated)
 
+        std::string line;
+
+        if(!recv_line(client_fd, line)) break; //the client disconnected 
+        // Split command and argument we need how to message will come
         std::string command;
         std::string arg;
-        size_t space_pos=line.find(' ');
+        size_t space_pos=line.find(' '); //find space to split command and argument
 
-        if(space_pos!=std::string::npos){
+        if(space_pos!=std::string::npos){ // if there is space we take the first half to be the commend and the second to be the argument
             command=line.substr(0,space_pos);
             arg=line.substr(space_pos+1);
 
         }else{
-            command=line;
+            command=line; //if the is no space the whole line is the command(like LIST or QUIT)
         }
 
-        if (command=="HELLO"){
-                username=arg;
-                if(username.empty()){
+        if (command=="HELLO"){ //first command to authenticate the user
+                username=arg; 
+                if(username.empty()){ //check if the username is missing
                     send_all(client_fd, "ERR PROTOCOL missing_username\n");
                     check_username=false;
-                    continue;
+                    continue; 
                 }
-                
+                //check if the username contains only letters
                 for (char c : username){
                     if(!isalpha(c)){
                         is_authenticated=false;
@@ -77,8 +85,9 @@ void handle_client(int client_fd, InventoryManager& inventory) {
                        
                     }
                 }
+                // If username is valid
                 if(check_username){
-                    is_authenticated=true;
+                    is_authenticated=true; //the user is valid
                     send_all(client_fd, "OK HELLO\n");
 
                 }
@@ -86,27 +95,33 @@ void handle_client(int client_fd, InventoryManager& inventory) {
                     
            
                 }  
+                // If the user is not authorized helps us to avoid to "skip" the HELLO command
                 else if(!is_authenticated){
                     send_all(client_fd, "ERR STATE not_authenticated\n");
                     continue;
+
             }
 
-                
-
+                //Show the list of items            
                 else if(command=="LIST"){
+                    // Get the list of items from inventory
                     std::string response=inventory.listItems();
+                    // Send the response to the client
                     send_all(client_fd,response+"\n");
             
                 }
+                // Borrow an item
                 else if(command=="BORROW"){
                     int itemID;
                     try{
-                        
+                        // Convert argument to integer
                         itemID=std::stoi(arg);
+                        // Attempt to borrow the item going to InventoryManager to check if is available if it is we use the borrow method in Item
                         inventory.borrowItem(itemID,username);
                         send_all(client_fd, "OK BORROW "+std::to_string(itemID)+"\n");
                     }
                     catch(const std::invalid_argument&){
+                        // if the argument is not a valid integer
                         send_all(client_fd, "ERR PROTOCOL invalid_id\n");
                     }
                 //  catch if item is not available
@@ -117,25 +132,30 @@ void handle_client(int client_fd, InventoryManager& inventory) {
                     
                     
                 }
+                // Return an item
                 else if(command=="RETURN"){
                     int itemID;
                     try{
+                        // Convert argument to integer
                         itemID=std::stoi(arg);
                         inventory.returnItem(itemID,username);
                         send_all(client_fd, "OK RETURN "+std::to_string(itemID)+"\n");
                     }
-
+                    // if the argument is not a valid integer
                     catch(const std::invalid_argument&){
                         send_all(client_fd, "ERR PROTOCOL invalid_id\n");
                     }
+                    //  if there is an error during return not the borrower or item not found
                     catch(const std::runtime_error& e){
                         send_all(client_fd, e.what());
                     }
                 }
+                // Wait until an item is available
                 else if(command=="WAIT"){
                     int itemID;
                     try{
                         itemID=std::stoi(arg);
+                        // the thread will wait until the item is available its not good in the real life scenario but for this project is ok(can make a long queue )
                         inventory.waitUntilAvailable(itemID,username);
                         send_all(client_fd, "OK WAIT "+std::to_string(itemID)+"\n");
                     }
@@ -153,6 +173,7 @@ void handle_client(int client_fd, InventoryManager& inventory) {
                 }
                 
             } 
+            // Close client connection
             close(client_fd);
             }
             
@@ -162,24 +183,30 @@ void handle_client(int client_fd, InventoryManager& inventory) {
 
 
 int main(int argc, char *argv){
+    // Create InventoryManager instance
     InventoryManager items;
-   
+    // Set up server socket
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-
+    // Set socket options to reuse address and port
     sockaddr_in addr{};
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    addr.sin_port = htons(5555);
-
+    addr.sin_port = htons(8080);
+    // binding the socket
     bind(server_fd, (sockaddr*)&addr, sizeof(addr));
+    
+    //  Start listening for incoming connections(10 is the max)(leon said that ben told him to do 20 we need to check with him)
     listen(server_fd, 10);
 
     std::cout << "Server listening on port 5555\n";
-
+    // Accept and handle client connections
     while (true) {
+        // Accept a new client connection
         int client_fd = accept(server_fd, nullptr, nullptr);
+        // Create a new thread to handle the client
         std::thread(handle_client, client_fd, std::ref(items)).detach();
     }
+    // close The server socket
     close(server_fd);
     return 0;
     
